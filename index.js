a1166aa16783c1b21ed96d98b3ef49ada650f405
Paulo Alexandre Machado <paulo.machado@sysmap.com.br>
2018-06-18 17:24:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+var red = rootStyles.getPropertyValue('--red');
+console.log('red: ', red);
+
+// root.style.setProperty('--red', 'green');
